25263b0647a31386689bc50197428422bd80da81
Sam <yz64@illinois.edu>
2023-04-26 17:59:29 +0800

    add gimbal skd

--- dev/application/param_adjusts/pa_hn/Gimbal_SKD.h
+++ dev/application/param_adjusts/pa_hn/Gimbal_SKD.h
@@ -5,11 +5,32 @@
 #ifndef META_EMBEDDED_GIMBAL_SKD_H
 #define META_EMBEDDED_GIMBAL_SKD_H
 
+#include "ch.hpp"
 
 class Gimbal_SKD {
 public:
+
+    enum motor_id{
+        Yaw,
+        Pitch,
+        gimbal_motor_count
+    };
+
     static void start();
+
+    static void set_yaw_angle();
+
+    static void set_pitch_angle();
+
+    static void set_gimbal_angle();
 private:
+    static float gimbal_feedback_angle[gimbal_motor_count];
+
+    static float gimbal_target_angle[gimbal_motor_count];
+
+    class SKDThread : public chibios_rt::BaseStaticThread<512> {
+        void main() final;
+    };
 };
 
 
